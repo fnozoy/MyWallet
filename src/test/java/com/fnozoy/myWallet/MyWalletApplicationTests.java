0b96b103edf59c171565c36621671abeb6373bd8
fnozoy <fnozoy@yahoo.com>
2021-07-03 08:22:44 -0300

    first commit with virgin project directly from start.spring.io

---
+++ src/test/java/com/fnozoy/myWallet/MyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnozoy.myWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
