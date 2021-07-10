99081d421c01002c82900de8761bd033834fc813
fnozoy <fnozoy@yahoo.com>
2021-07-10 07:38:10 -0300

    fix cors and let tests run

--- src/main/java/com/fnozoy/myWallet/MyWalletApplication.java
+++ src/main/java/com/fnozoy/myWallet/MyWalletApplication.java
@@ -2,10 +2,22 @@ package com.fnozoy.myWallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MyWalletApplication {
+public class MyWalletApplication{
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyWalletApplication.class, args);
 	}
